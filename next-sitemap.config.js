c084a427a112cac6ca7dd827c4e54e48b2db5eb4
ToastedAvenger <s.m.maazulhaq@gmail.com>
2025-07-15 09:13:10 +0500

    Sitemap Fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,18 @@
+// next-sitemap.config.js
 export default {
   siteUrl: 'https://teamenvision.pk',
   generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  outDir: './public',
+  sitemapSize: 7000, // default is fine
+  autoLastmod: true,
+  // Manually define routes (for now)
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      lastmod: new Date().toISOString(),
+      changefreq: 'monthly',
+      priority: 0.7,
+    };
+  },
 };
